632d972d336652938bc7dd1dc85a936f52eb0cfb
Niati <niatiniati545@gmail.com>
2024-10-18 22:37:16 +0700

    Perubahan alamat dan sebagian kecil pada bagian profile trainee

--- resources/views/peserta/PengaturanPeserta.blade.php
+++ resources/views/peserta/PengaturanPeserta.blade.php
@@ -4,7 +4,7 @@
 
 <!-- start formulir -->
 <section class="bg-white dark:bg-gray-900">
-  <form action="" class="pb-8 pt-2 px-4">
+  <form action="" class="pb-2 pt-2 px-4">
     <!-- photo start -->
       <div class=" flex mb-2">
         <div>
@@ -22,10 +22,10 @@
         <button type="button" class=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-6 py-1.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Edit</button>
     </div>
 
-    <div class=" flex gap-6 ">
+    <div class=" flex gap-6 mb-5">
         <div class="  w-full">
             <label for="telepon" class="block mb-2 text-sm font-bold text-gray-900 dark:text-white">No Telepon</label>
-            <input type="text" name="telpon" id="telpon" class="bg-gray-100 border border-gray-100 text-gray-900 mb-5 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your phone number" required="">
+            <input type="text" name="telpon" id="telpon" class="bg-gray-100 border border-gray-100 text-gray-900 mb-10 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your phone number" required="">
 
             <label for="jeniskelamin" class="block mb-2 text-sm font-bold text-gray-900 dark:text-white">Jenis Kelamin</label>
             <select id="jeniskelamin" class="bg-gray-100 border border-gray-100 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 mb-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
@@ -54,7 +54,7 @@
         </div>
         <div class="  w-full">
             <label for="alamat" class="block mb-2 text-sm font-bold text-gray-900 dark:text-white">Alamat</label>
-            <input type="text" name="alamat" id="alamat" class="bg-gray-100 border border-gray-100 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 mb-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your address" required="">
+            <textarea name="alamat" id="alamat" class=" bg-gray-100 border border-gray-100 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 mb-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your address" required=""></textarea>
 
             <label for="education" class="block mb-2 text-sm font-bold text-gray-900 dark:text-white">Education</label>
             <input type="text" name="education" id="education" class="bg-gray-100 border border-gray-100 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 mb-20 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your education" required="">
@@ -71,7 +71,7 @@
     </div>
 
     <div class="flex justify-end w-full">
-        <button type="button" class=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-6 py-1.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Save</button>
+        <button type="button" class=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-10 py-1.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Save</button>
     </div>
     
     <!-- bagian formulir -->
