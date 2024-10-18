8d8cdec8ac5b6a58df3ce228ec645a209677d9f3
Niati <niatiniati545@gmail.com>
2024-10-18 22:40:26 +0700

    disabel option tambah sertifikat

--- resources/views/pelatih/TambahSertifikat.blade.php
+++ resources/views/pelatih/TambahSertifikat.blade.php
@@ -19,7 +19,7 @@
         <form class="mt-5 w-full">
             <label for="courses" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pilih Kursus untuk Sertifikat</label>
             <select id="courses" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                <option>Pilih Kursus</option>
+                <option selected disabled>Pilih Kursus</option>
                 <option value="course1">Kursus Welding</option>
                 <option value="course2">Kursus Fitting</option>
                 <option value="course3">Kursus Painting</option>
