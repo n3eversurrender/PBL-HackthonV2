c5a824d5b2d62117a8505c1dd3088bb7cc2505c0
Zaini Ridha <zainiridha8@Gmail.com>
2024-11-25 09:24:31 +0700

    update sertifikat

--- app/Http/Controllers/DataSertifikatController.php
+++ app/Http/Controllers/DataSertifikatController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Sertifikat;
 use Illuminate\Http\Request;
 
 class DataSertifikatController extends Controller
 {
-    //
+    public function dataSertifikat()
+    {
+        // Ambil semua sertifikat tanpa eager loading
+        $sertifikat = Sertifikat::paginate(10);
+
+        return view('Admin.DataSertifikat', [
+            'sertifikat' => $sertifikat,
+        ]);
+    }
+
+    // Menghapus sertifikat dari database
+    public function destroy($sertifikat_id)
+    {
+        // Mengambil data sertifikat berdasarkan id
+        $sertifikat = Sertifikat::findOrFail($sertifikat_id);
+
+        // Hapus file sertifikat jika ada
+        if (file_exists(storage_path('app/sertifikat_pdfs/' . $sertifikat->file_sertifikat))) {
+            unlink(storage_path('app/sertifikat_pdfs/' . $sertifikat->file_sertifikat));  // Hapus file sertifikat
+        }
+
+        // Hapus sertifikat dari database
+        $sertifikat->delete();
+
+        // Redirect kembali ke halaman pengelolaan sertifikat dengan pesan sukses
+        return redirect()->back()->with('success', 'Sertifikat berhasil dihapus.');
+    }
 }
