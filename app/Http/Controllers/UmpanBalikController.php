4d5074fef3398299687bf5adf89cd81064c7d496
Zaini Ridha <zainiridha8@Gmail.com>
2024-11-25 08:44:18 +0700

    update data umpan balik

---
+++ app/Http/Controllers/UmpanBalikController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UmpanBalik;
+use Illuminate\Http\Request;
+
+class UmpanBalikController extends Controller
+{
+    public function store(Request $request)
+    {
+        // Validate the incoming data
+        $validated = $request->validate([
+            'nama_komentar' => 'required|string|max:255',
+            'komentar' => 'required|string',
+        ]);
+
+        UmpanBalik::create([
+            'nama_komentar' => $validated['nama_komentar'],
+            'komentar' => $validated['komentar'],
+        ]);
+
+        return redirect()->back()->with('success', 'Komentar berhasil dikirim!');
+    }
+}
