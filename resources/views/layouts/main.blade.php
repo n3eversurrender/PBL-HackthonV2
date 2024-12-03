458fcac3d6177c7bd2014eb987754a5b27e574b7
Niati <niatiniati545@gmail.com>
2024-12-03 23:12:24 +0700

    Perbaikan Navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,112 +13,123 @@
 
 <body class="w-full">
     <!-- Navbar + Image -->
-    <nav class="bg-black bg-opacity-30 border-gray-200 dark:bg-black dark:bg-opacity-50 fixed w-full z-10">
-        <div class="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+    <nav class="bg-black bg-opacity-70 border-gray-200 dark:bg-black dark:bg-opacity-50 fixed w-full z-10">
+        <div class="max-w-screen-2xl grid grid-cols-12 items-center sm:justify-between mx-auto p-2 sm:p-4">
+            <a href="#" class="flex {{ Auth::check() ? 'col-span-9' : 'col-span-8' }} sm:col-span-5 items-center space-x-2 rtl:space-x-reverse">
                 <img src="{{ asset('image/SKILLB.png') }}" class="w-10 h-10 rounded-full" alt="Flowbite Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">SKILL BRIDGE</span>
+                <span class="self-center text-lg sm:text-2xl font-semibold whitespace-nowrap text-white">SKILL BRIDGE</span>
             </a>
-            <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                @if (Auth::check())
-                @php
-                $user = Auth::user(); // Ambil data pengguna yang sedang login
-                @endphp
+            
+           <div class="{{ Auth::check() ? 'col-span-3' : 'col-span-4' }} flex  gap-x-3  sm:col-span-7 sm:flex sm:justify-between">
+                <div class="items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                    @if (Auth::check())
+                    @php
+                    $user = Auth::user(); // Ambil data pengguna yang sedang login
+                    @endphp
 
-                <!-- Menampilkan nama pengguna -->
-                <!-- <div>
-                    <h3 class="text-white">Selamat datang, {{ $user->nama }}!</h3>
-                </div> -->
+                    <!-- Menampilkan nama pengguna -->
+                    <!-- <div>
+                        <h3 class="text-white">Selamat datang, {{ $user->nama }}!</h3>
+                    </div> -->
 
-                <!-- Menampilkan menu berdasarkan peran -->
-                @if($user->peran === 'Pelatih')
-                <!-- Menu untuk Pelatih -->
-                <button type="button" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-                    <span class="sr-only">Open user menu</span>
-                    <img class="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src="{{ $user->foto_profil ? asset('storage/foto_profil/' . $user->foto_profil) : asset('image/9203764.png') }}" alt="user photo">
-                </button>
+                    <!-- Menampilkan menu berdasarkan peran -->
+                    @if($user->peran === 'Pelatih')
+                    <!-- Menu untuk Pelatih -->
+                    <button type="button" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                        <span class="sr-only">Open user menu</span>
+                        <img class="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src="{{ $user->foto_profil ? asset('storage/foto_profil/' . $user->foto_profil) : asset('image/9203764.png') }}" alt="user photo">
+                    </button>
 
-                <div class="z-50 block my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
-                    <div class="px-4 py-3">
-                        <span class="block text-sm text-gray-900 dark:text-white">{{ $user->nama }}</span>
-                        <span class="block text-sm text-gray-500 truncate dark:text-gray-400">{{ $user->email }}</span>
+                    <div class="z-50 block my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+                        <div class="px-4 py-3">
+                            <span class="block text-sm text-gray-900 dark:text-white">{{ $user->nama }}</span>
+                            <span class="block text-sm text-gray-500 truncate dark:text-gray-400">{{ $user->email }}</span>
+                        </div>
+                        <ul class="py-2" aria-labelledby="user-menu-button">
+                            <li>
+                                <a href="{{ route('DashboardPelatih') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard Pelatih</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('PengaturanPelatih') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Pengaturan Pelatih</a>
+                            </li>
+                            <li>
+                                <form action="{{ route('logoutPelatih') }}" method="POST">
+                                    @csrf
+                                    <button type="submit" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                                        Keluar
+                                    </button>
+                                </form>
+                            </li>
+                        </ul>
                     </div>
-                    <ul class="py-2" aria-labelledby="user-menu-button">
+                    @elseif($user->peran === 'Peserta')
+                    <!-- Menu untuk Peserta -->
+                    <button type="button" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                        <span class="sr-only">Open user menu</span>
+                        <img class="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src="{{ $user->foto_profil ? asset('storage/foto_profil/' . $user->foto_profil) : asset('image/9203764.png') }}" alt="user photo">
+                    </button>
+
+                    <div class="z-50 block my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+                        <div class="px-4 py-3">
+                            <span class="block text-sm text-gray-900 dark:text-white">{{ $user->nama }}</span>
+                            <span class="block text-sm text-gray-500 truncate dark:text-gray-400">{{ $user->email }}</span>
+                        </div>
+                        <ul class="py-2" aria-labelledby="user-menu-button">
+                            <li>
+                                <a href="{{ route('DashboardPeserta') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard Peserta</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('PengaturanPeserta') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Pengaturan Peserta</a>
+                            </li>
+                            <li>
+                                <form action="{{ route('logoutPeserta') }}" method="POST">
+                                    @csrf
+                                    <button type="submit" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                                        Keluar
+                                    </button>
+                                </form>
+                            </li>
+                        </ul>
+                    </div>
+                    @endif
+                    @else
+                    <!-- Tampilkan tombol login jika pengguna belum login -->
+                    <button type="button" id="login-button" class=" mt-1.5 text-gray-900 bg-white  hover:bg-HoverGlow hover:text-white transition duration-500 font-medium rounded-lg text-xs sm:text-sm lg:px-5 px-3 lg:py-2.5 py-2 me-2 mb-2 group" onclick="window.location.href='/Masuk';">
+                        Masuk
+                    </button>
+                    @endif
+
+                </div>
+
+                <button data-collapse-toggle="navbar-default" type="button" class="inline-flex mt-2 z-20 items-center p-1 w-7 h-7 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                    </svg>
+                </button>
+            
+                <div class="items-center absolute top-14 sm:top-0 right-0 z-10 sm:relative justify-between hidden sm:w-full md:flex md:w-auto md:order-1" id="navbar-default">
+                    <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-transparent md:dark:bg-transparent dark:border-gray-700">
                         <li>
-                            <a href="{{ route('DashboardPelatih') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard Pelatih</a>
+                            <a href="/Home"
+                                class="{{ request()->is('Home') ? ' text-blue-700 font-bold' : 'text-black sm:text-white' }} block py-2 px-3 rounded md:border-0 md:p-0 hover:text-blue-500">
+                                Beranda
+                            </a>
                         </li>
                         <li>
-                            <a href="{{ route('PengaturanPelatih') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Pengaturan Pelatih</a>
+                            <a href="/TentangKami"
+                                class="{{ request()->is('TentangKami') ? ' text-blue-700 font-bold' : 'text-black sm:text-white' }} block py-2 px-3 rounded md:border-0 md:p-0 hover:text-blue-500">
+                                Tentang Kami
+                            </a>
                         </li>
                         <li>
-                            <form action="{{ route('logoutPelatih') }}" method="POST">
-                                @csrf
-                                <button type="submit" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-                                    Keluar
-                                </button>
-                            </form>
+                            <a href="/DaftarKursus"
+                                class="{{ request()->is('DaftarKursus') ? ' text-blue-700 font-bold' : 'text-black sm:text-white' }} block py-2 px-3 rounded md:border-0 md:p-0 hover:text-blue-500">
+                                Kursus
+                            </a>
                         </li>
                     </ul>
                 </div>
-                @elseif($user->peran === 'Peserta')
-                <!-- Menu untuk Peserta -->
-                <button type="button" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-                    <span class="sr-only">Open user menu</span>
-                    <img class="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src="{{ $user->foto_profil ? asset('storage/foto_profil/' . $user->foto_profil) : asset('image/9203764.png') }}" alt="user photo">
-                </button>
-
-                <div class="z-50 block my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
-                    <div class="px-4 py-3">
-                        <span class="block text-sm text-gray-900 dark:text-white">{{ $user->nama }}</span>
-                        <span class="block text-sm text-gray-500 truncate dark:text-gray-400">{{ $user->email }}</span>
-                    </div>
-                    <ul class="py-2" aria-labelledby="user-menu-button">
-                        <li>
-                            <a href="{{ route('DashboardPeserta') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard Peserta</a>
-                        </li>
-                        <li>
-                            <a href="{{ route('PengaturanPeserta') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Pengaturan Peserta</a>
-                        </li>
-                        <li>
-                            <form action="{{ route('logoutPeserta') }}" method="POST">
-                                @csrf
-                                <button type="submit" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-                                    Keluar
-                                </button>
-                            </form>
-                        </li>
-                    </ul>
-                </div>
-                @endif
-                @else
-                <!-- Tampilkan tombol login jika pengguna belum login -->
-                <button type="button" id="login-button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" onclick="window.location.href='/Masuk';">
-                    Masuk
-                </button>
-                @endif
-
-            </div>
-            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
-                <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-transparent md:dark:bg-transparent dark:border-gray-700">
-                    <li>
-                        <a href="/Home"
-                            class="{{ request()->is('Home') ? 'border-b-2 border-blue-700 text-blue-700 font-bold' : 'text-white' }} block py-2 px-3 rounded md:border-0 md:p-0 hover:text-blue-500">
-                            Beranda
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/TentangKami"
-                            class="{{ request()->is('TentangKami') ? 'border-b-2 border-blue-700 text-blue-700 font-bold' : 'text-white' }} block py-2 px-3 rounded md:border-0 md:p-0 hover:text-blue-500">
-                            Tentang Kami
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/DaftarKursus"
-                            class="{{ request()->is('DaftarKursus') ? 'border-b-2 border-blue-700 text-blue-700 font-bold' : 'text-white' }} block py-2 px-3 rounded md:border-0 md:p-0 hover:text-blue-500">
-                            Kursus
-                        </a>
-                    </li>
-                </ul>
             </div>
         </div>
     </nav>
