3979b9a719c15e307b12e90c222de2dbb17c9f41
Niati <niatiniati545@gmail.com>
2024-12-04 22:09:02 +0700

    Responsive halaman daftar

--- resources/views/guest/Daftar.blade.php
+++ resources/views/guest/Daftar.blade.php
@@ -12,33 +12,38 @@
 </head>
 
 <body>
-    <div class="flex justify-between h-full">
-        <div class="bg-white px-24 p-4 w-full">
-            <h2 class="font-bold text-3xl text-center mb-10">Buat Akun Anda</h2>
-            <form id="registerForm" action="{{ route('pengguna.store') }}" method="POST">
+    <div class="flex flex-col lg:flex-row lg:justify-between h-full">
+    
+        <img class="w-full h-48 sm:h-80 object-cover lg:w-1/3 lg:h-auto" 
+            src="{{ asset('image/12.webp') }}" 
+            alt="Background Main" />
+
+        <div class="bg-white p-4 w-full mt-5  lg:mt-0 mb-10 flex flex-col justify-center items-center">
+            <h2 class="font-bold text-2xl sm:text-3xl lg:text-4xl text-center my-2 lg:my-10 w-full">Buat Akun Anda</h2>
+            <form id="registerForm" action="{{ route('pengguna.store') }}" method="POST" class="w-full px-4 lg:px-0 mt-2 sm:mt-5 lg:mt-0 lg:max-w-md">
                 @csrf
                 <div class="grid gap-6 mb-6 md:grid-cols-2">
                     <div>
                         <label for="nama" class="block mb-2 text-sm font-semibold text-gray-900 dark:text-white">Nama</label>
-                        <input type="text" id="nama" name="nama" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John Doe" required />
+                        <input type="text" id="nama" name="nama" class="bg-gray-100 border border-Border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="John Doe" required />
                     </div>
                     <div>
                         <label for="email" class="block mb-2 text-sm font-semibold text-gray-900 dark:text-white">Email</label>
-                        <input type="email" id="email" name="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="john.doe@company.com" required />
+                        <input type="email" id="email" name="email" class="bg-gray-100 border border-Border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="john.doe@company.com" required />
                     </div>
                 </div>
                 <div class="mb-6">
                     <label for="no_telepon" class="block mb-2 text-sm font-semibold text-gray-900 dark:text-white">No Telepon</label>
-                    <input type="text" id="no_telepon" name="no_telepon" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="08123300220" required />
+                    <input type="number" id="no_telepon" name="no_telepon" class="bg-gray-100 border border-Border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="08123300220" required />
                 </div>
                 <div class="mb-6">
                     <label for="alamat" class="block mb-2 text-sm font-semibold text-gray-900 dark:text-white">Alamat</label>
-                    <textarea id="alamat" name="alamat" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Masukkan alamat lengkap..."></textarea>
+                    <textarea id="alamat" name="alamat" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-Border focus:ring-blue-500 focus:border-blue-500" placeholder="Masukkan alamat lengkap..."></textarea>
                 </div>
                 <div class="grid gap-6 mb-6 md:grid-cols-2">
                     <div>
                         <label for="jenis_kelamin" class="block mb-2 text-sm font-semibold text-gray-900 dark:text-white">Jenis Kelamin</label>
-                        <select id="jenis_kelamin" name="jenis_kelamin" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                        <select id="jenis_kelamin" name="jenis_kelamin" class="bg-gray-100 border border-Border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                             <option selected>Pilih Jenis Kelamin</option>
                             <option value="Laki-laki">Laki-Laki</option>
                             <option value="Perempuan">Perempuan</option>
@@ -46,7 +51,7 @@
                     </div>
                     <div>
                         <label for="peran" class="block mb-2 text-sm font-semibold text-gray-900 dark:text-white">Role</label>
-                        <select id="peran" name="peran" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                        <select id="peran" name="peran" class="bg-gray-100 border border-Border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                             <option selected>Pilih Role</option>
                             <option value="Peserta">Peserta</option>
                             <option value="Pelatih">Pelatih</option>
@@ -54,26 +59,35 @@
                     </div>
                 </div>
                 <div class="grid gap-6 mb-6 md:grid-cols-2">
-                    <div class="mb-6">
+                    <div class="lg:mb-3">
                         <label for="kata_sandi" class="block mb-2 text-sm font-semibold text-gray-900 dark:text-white">Password</label>
                         <div class="relative">
-                            <input type="password" id="kata_sandi" name="kata_sandi" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pr-10" placeholder="•••••••••" required />
+                            <input type="password" id="kata_sandi" name="kata_sandi" class="bg-gray-100 border border-Border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pr-10" placeholder="•••••••••" required />
                             <i id="togglePassword" class="fas fa-eye absolute top-1/2 right-3 transform -translate-y-1/2 cursor-pointer"></i>
                         </div>
                     </div>
 
-                    <div class="mb-6">
+                    <div class="lg:mb-3">
                         <label for="confirm_kata_sandi" class="block mb-2 text-sm font-semibold text-gray-900 dark:text-white">Konfirmasi Password</label>
                         <div class="relative">
-                            <input type="password" id="confirm_kata_sandi" name="confirm_kata_sandi" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pr-10" placeholder="•••••••••" required />
+                            <input type="password" id="confirm_kata_sandi" name="confirm_kata_sandi" class="bg-gray-100 border border-Border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pr-10" placeholder="•••••••••" required />
                             <i id="toggleConfirmPassword" class="fas fa-eye absolute top-1/2 right-3 transform -translate-y-1/2 cursor-pointer"></i>
                         </div>
                     </div>
                 </div>
-                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Kirim</button>
+                <div class="flex justify-center sm:mt-8 mt-0 lg:mt-0 ">
+                    <button type="submit" class="text-white bg-ButtonBase hover:bg-HoverGlow transition duration-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-lg text-sm w-full sm:w-1/2 lg:w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Kirim</button>
+                </div>
             </form>
+
+            <a href="/Masuk" class="group font-semibold text-sm sm:text-base mt-2 text-ButtonBase transition duration-700">
+                <span class="group-hover:text-HoverGlow text-slate-950">Sudah Punya Akun?</span>
+                <span class="group-hover:text-HoverGlow">Masuk</span>
+            </a>
+
+            <a href="/Home" class="text-sm sm:text-base font-semibold mt-7 text-slate-800 hover:text-HoverGlow transition duration-700">Kembali Ke Beranda</a>
         </div>
-        <img class="h-auto w-1/3 object-cover" src="{{ asset('image/12.webp') }}" alt="Background Main">
+       
     </div>
 
     <script>
--- resources/views/guest/Masuk.blade.php
+++ resources/views/guest/Masuk.blade.php
@@ -10,30 +10,11 @@
 </head>
 
 <body>
-    <!-- <div class="flex justify-between h-screen">
-        <div class="bg-white px-24 p-4 w-full flex flex-col justify-center items-center">
-            <h2 class="font-bold text-3xl text-center mb-10">Ayo Segera Masuk Ke Halamanmu</h2>
-            <form method="POST" action="{{ route('login') }}" class="w-full max-w-md">
-                @csrf
-                <div class="mb-6">
-                    <label for="email" class="block mb-2 text-sm font-semibold text-gray-900 dark:text-white">Email</label>
-                    <input type="email" id="email" name="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" required />
-                </div>
-                <div class="mb-6 relative">
-                    <label for="kata_sandi" class="block mb-2 text-sm font-semibold text-gray-900 dark:text-white">Kata Sandi</label>
-                    <input type="password" id="kata_sandi" name="kata_sandi" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required />
-                    <i class="fas fa-eye absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer" id="togglePassword"></i>
-                </div>
-                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Masuk</button>
-            </form>
-        </div>
-        <img class="h-auto w-1/3 object-cover" src="{{ asset('image/12.webp') }}" alt="Background Main">
-    </div> -->
     <div class="flex flex-col lg:flex-row lg:justify-between h-screen">
     <!-- Image Section -->
     <img class="w-full h-48 sm:h-1/3 object-cover lg:w-1/3 lg:h-auto" 
          src="{{ asset('image/12.webp') }}" 
-         alt="Background Main">
+         alt="Background Main" />
     
     <!-- Form Section -->
     <div class="bg-white p-4 w-full mt-5 sm:mt-14 lg:mt-0 flex flex-col justify-center items-center">
